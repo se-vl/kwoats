ad36cfd9fb01365bd6b14ef633eb5ea8fa26e267
se-vl <se-vl@users.noreply.github.com>
2017-06-15 18:48:08 +0200

    extract QuoteService from QuoteServlet

---
+++ src/main/java/kwoats/QuoteService.java
@@ -0,0 +1,35 @@
+package kwoats;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class QuoteService {
+	private static final String[] wordsOfWisdom = {
+			"Multicore processors are just now [May 2006] becoming inexpensive enough for midrange desktop systems. Not coincidentally, many development teams are noticing more and more threading-related bug reports in their projects.",
+			"In a recent post on the Netbeans developer site, one of the core maintainers observed that a single class had been patched over 14 times to fix threading-related problems.",
+			"Dion Almaer, former editor of TheServerSide, recently blogged (after a painful debugging session that ultimately revealed a threading bug) that most Java programs are so rife with concurrency bugs that they work only 'by accident'.",
+			"It is all too easy to create programs that compile and appear to work but are nevertheless broken.",
+			"It would be nice to believe that concurrency is an 'optional' or 'advanced' language feature, but the reality is that nearly all Java applications are multithreaded and these frameworks do not insulate you from the need to properly coordinate access to application state.",
+			"Whenever more than one thread accesses a given state variable, and one of them might write to it, they all must coordinate their access to it using synchronization. You should avoid the temptation to think that there are 'special' situations in which this rule does not apply.",
+			"A program that omits needed synchronization might appear to work, passing its tests and performing well for years, but it is still broken and may fail at any moment.", };
+
+	private List<String> allQuotes = new ArrayList<>(Arrays.asList(wordsOfWisdom));
+
+	public int numberOfQuotes() {
+		return allQuotes.size();
+	}
+
+	public String getQuoteAt(int randomIndex) {
+		return allQuotes.get(randomIndex);
+	}
+
+	public void addNewQuote(String newQuote) {
+		allQuotes.add(newQuote);
+	}
+
+	public List<String> viewAllQuotes() {
+		return Collections.unmodifiableList(allQuotes);
+	}
+}
--- src/main/java/kwoats/QuoteServlet.java
+++ src/main/java/kwoats/QuoteServlet.java
@@ -1,9 +1,6 @@
 package kwoats;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -17,27 +14,17 @@ import javax.servlet.http.HttpSession;
 public class QuoteServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	private static final String[] wordsOfWisdom = {
-			"Multicore processors are just now [May 2006] becoming inexpensive enough for midrange desktop systems. Not coincidentally, many development teams are noticing more and more threading-related bug reports in their projects.",
-			"In a recent post on the Netbeans developer site, one of the core maintainers observed that a single class had been patched over 14 times to fix threading-related problems.",
-			"Dion Almaer, former editor of TheServerSide, recently blogged (after a painful debugging session that ultimately revealed a threading bug) that most Java programs are so rife with concurrency bugs that they work only 'by accident'.",
-			"It is all too easy to create programs that compile and appear to work but are nevertheless broken.",
-			"It would be nice to believe that concurrency is an 'optional' or 'advanced' language feature, but the reality is that nearly all Java applications are multithreaded and these frameworks do not insulate you from the need to properly coordinate access to application state.",
-			"Whenever more than one thread accesses a given state variable, and one of them might write to it, they all must coordinate their access to it using synchronization. You should avoid the temptation to think that there are 'special' situations in which this rule does not apply.",
-			"A program that omits needed synchronization might appear to work, passing its tests and performing well for years, but it is still broken and may fail at any moment.", };
-
-	private List<String> allQuotes = new ArrayList<>(Arrays.asList(wordsOfWisdom));
-
 	private RandomNumberService randomNumberService = new RandomNumberService();
+	private QuoteService quoteService = new QuoteService();
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		HttpSession session = request.getSession();
 		Integer previousRandomIndex = (Integer) session.getAttribute("previousRandomIndex");
 
-		int numberOfQuotes = allQuotes.size();
+		int numberOfQuotes = quoteService.numberOfQuotes();
 		int randomIndex = randomNumberService.pickRandomNumber(numberOfQuotes, previousRandomIndex);
-		String randomQuote = allQuotes.get(randomIndex);
+		String randomQuote = quoteService.getQuoteAt(randomIndex);
 
 		session.setAttribute("previousRandomIndex", randomIndex);
 
@@ -54,10 +41,10 @@ public class QuoteServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		String newQuote = request.getParameter("newQuote");
-		allQuotes.add(newQuote);
+		quoteService.addNewQuote(newQuote);
 
 		// fill the model
-		request.setAttribute("allQuotes", allQuotes);
+		request.setAttribute("allQuotes", quoteService.viewAllQuotes());
 
 		// forward to the view
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/AllQuotes.jsp");
