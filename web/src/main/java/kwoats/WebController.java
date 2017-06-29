2fb4801d6242804e243bdf968407d46dbb35f61d
se-vl <se-vl@users.noreply.github.com>
2017-06-29 21:45:20 +0200

    rename QuoteController to WebController

--- web/src/main/java/kwoats/WebController.java
+++ web/src/main/java/kwoats/WebController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class QuoteController {
+public class WebController {
 	@Autowired
 	private RandomNumberClient randomNumberClient;
 
